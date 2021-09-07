228488c74261794c2355ce8fe01b4ab07a2e1612
FicoHu <flyinghyp@outlook.com>
2021-09-07 10:23:44 +0800

    set_reuseport 测试，端口冲突

--- net/src/listener.rs
+++ net/src/listener.rs
@@ -3,7 +3,10 @@ use super::Stream;
 
 use std::io::Error;
 use std::io::ErrorKind;
+use std::str::FromStr;
 
+use tokio::io;
+use tokio::net::TcpSocket;
 use tokio::net::{TcpListener, UnixListener};
 
 pub enum Listener {
@@ -15,9 +18,20 @@ impl Listener {
     pub async fn bind(protocol: &str, addr: &str) -> std::io::Result<Self> {
         match protocol {
             "unix" => Ok(Listener::Unix(UnixListener::bind(addr)?)),
-            "tcp" => Ok(Listener::Tcp(TcpListener::bind(addr).await?)),
+            "tcp" => Ok(Listener::Tcp(Listener::listen_reuseport(addr).await?)),
             _ => Err(Error::new(ErrorKind::InvalidInput, addr)),
         }
+    }
+
+    pub async fn listen_reuseport(addr: &str) -> io::Result<TcpListener> {
+        let addr = std::net::SocketAddr::from_str(addr).unwrap();
+        let socket = match addr {
+            std::net::SocketAddr::V4(_) => TcpSocket::new_v4(),
+            std::net::SocketAddr::V6(_) => TcpSocket::new_v6(),
+        }?;
+        socket.set_reuseport(true)?;
+        socket.bind(addr)?;
+        Ok(socket.listen(1024)?)
     }
 
     pub async fn accept(&self) -> std::io::Result<(Stream, SocketAddr)> {
